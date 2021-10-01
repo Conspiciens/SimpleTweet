e0d915c7867df5f1166f2743999ca2b76a04f3ca
Conspiciens <fernandezjohann08@gmail.com>
2021-10-01 05:35:33 -0700

    Added tweeting features

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,16 +1,16 @@
 package com.codepath.apps.restclienttemplate.models;
 
-import android.os.Parcel;
-
 import com.codepath.apps.restclienttemplate.TimeFormatter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Tweet {
 
     public String body;
@@ -18,6 +18,8 @@ public class Tweet {
     public long id;
     public User user;
     // public String date;
+
+    public Tweet() {}
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
